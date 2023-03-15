c55d77d5ff0f808bc7fe5f620201cb36aabf2a9a
MICHELL CRUZ (MACP) <123117399+Michell0605@users.noreply.github.com>
2023-03-15 14:29:29 -0600

    Add files via upload

---
+++ private/modulos/docente/docente.php
@@ -0,0 +1,45 @@
+<?php
+include '../../config/config.php';
+extract($REQUEST);
+
+$docentes = isset($docentes) ? $docentes : '[]';
+$accion = isset($accion) ? $accion : '';
+
+class Docente{
+    private $datos=[], $db, $respuesta=['msg'=>'ok'];
+
+    public function __construct($db){
+        $this->db =$db;
+    }
+    public function recibir_datos($docente){
+        $this->datos = json?decode($docente, true);
+        return $this->validar_datos();
+    }
+    private function validar_datos(){
+        if(empty($this->datos['idDocente'])){
+            $this->respuesta['msg'] = 'No se pudo sellecionar ID';
+        }
+        if(empty($this->datos['codigo'])){
+            $this->respuesta['msg'] = 'Por favor ingrese el codigo';
+        }
+        if(empty($this->datos['nombre'])){
+            $this->respuesta['msg'] = 'Por favor ingrese el nombre';
+        }
+        return $this->administrar_docente();
+    }
+    private function administrar_docente(){
+        global $accion;
+        if( $this->respuesta['msg']=='ok' ){
+            if($accion=='nuevo'){
+
+            }else if($accion=='modificar'){
+                
+            }
+
+        }else{
+            return $this->respuesta;
+        }
+    }
+
+}
+?>
